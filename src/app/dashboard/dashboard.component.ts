e6cabbd088d450049c5a0e152db8aad18f209b80
Anderson Alves <andersonalves4608@gmail.com>
2019-08-27 15:08:05 -0300

    Configurando tela inicial

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  public formSearch: FormGroup;
+  public tes: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router ) { }
 
   ngOnInit() {
+    this.formSearch = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      inlineRadioOptions: new FormControl(''),
+    });
+  }
+  filterOptions() {
+    if (this.formSearch.value.inlineRadioOptions === 'author') {
+      this.router.navigate([`author/list/${this.formSearch.value.name}`]);
+    } else {
+      this.router.navigate([`book/list/${this.formSearch.value.name}`]);
+    }
   }
 
 }
