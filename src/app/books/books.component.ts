2defb5aaabbd36e19e36f18181271a1c11d23024
Anderson Alves <andersonalves4608@gmail.com>
2019-09-04 14:49:47 -0300

    Adicionando paginação aos livros

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -4,15 +4,35 @@ import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
+  styles: [
+    `
+      .main-panel {
+        height: 300px;
+        overflow-y: scroll;
+      }
+    `]
 })
 export class BooksComponent implements OnInit {
-  public book: any;
-
   constructor(private http: HttpClient ) { }
 
+  public static i = 2;
+  selector = '.main-panel';
+  public book: any;
+
+  onScrollDown() {
+    if (this.book[BooksComponent.i - 2].next === null) {
+      stop();
+    } else {
+      this.http.get(`${this.book[BooksComponent.i - 2].next}`).toPromise().then( ret => {
+        this.book.push(ret);
+      });
+      BooksComponent.i +=  1;
+    }
+  }
+
   ngOnInit() {
-    this.http.get('http://localhost:8000/v1/book').toPromise().then( ret => {
+    this.http.get('http://localhost:8000/v1/book/?page=1').toPromise().then( ret => {
       this.book = [ret];
     });
   }
